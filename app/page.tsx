adbf21c65c5049b760cae8d22869892d67a142e6
Aislã Lucio <aislanarchy@gmail.com>
2025-11-10 17:16:19 -0300

    feat: add header and image components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Header from "./_components/header";
+import SearchInput from "./_components/search-input";
+import banner from "../public/banner.png";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+      <div className="space-y-4 px-5">
+        <SearchInput />
+        <Image
+          src={banner}
+          alt="Agende agora!"
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </div>
   );
 }
